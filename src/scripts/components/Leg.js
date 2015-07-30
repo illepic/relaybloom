677dcaaa9a3300e5e3bffa6bf467ea5a0fd46509
Chris Bloom <bloomcb@gmail.com>
2015-07-30 16:05:54 -0700

    Leg progress

--- src/scripts/components/Leg.js
+++ src/scripts/components/Leg.js
@@ -7,9 +7,8 @@ export default class Leg extends React.Component {
   render() {
     return (
       <li className="list-group-item">
-        <span className="label label-default">{this.props.data.abbreviation}</span> <span>{this.props.data.racer.name}</span> <span className="badge">00:35:32 / {this.props.data.timeExpected}</span>
+        <span className="badge">00:35:32 / {this.props.data.timeExpected}</span><span className="label label-default">{this.props.data.abbreviation}</span> <span>{this.props.data.racer.name}</span>
         <br/>
-        <span className="pull-right expected-finish-time">3:17 pm</span>
       </li>
     );
   }
