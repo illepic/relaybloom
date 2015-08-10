6f109f6a4c8ddb51feaa17136d2694d67df035c0
Chris Bloom <bloomcb@gmail.com>
2015-08-09 19:22:17 -0700

    update packages, Leg styling with progress bars

--- app/components/Leg/Leg.jsx
+++ app/components/Leg/Leg.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import {ProgressBar} from 'react-bootstrap';
 import Timer from './../Timer';
 
 require('./leg.scss');
@@ -14,11 +15,14 @@ export default class Leg extends React.Component {
     let activeClass = (this.props.currentLeg === this.props.legData.legId) ? 'list-group-item-success' : null;
     return (
       <div className="leg">
-        <span className="label label-default left__label">{this.props.legData.abbreviation}</span>
-        <span className="leg__racer-name">{this.props.legData.racer.name}, {activeClass}</span>
-        <span className="badge leg__timer">
-          <Timer tickTime={this.props.legData.timeCompleted} totalTime={this.props.legData.timeExpected}/>
-        </span>
+        <div className="leg__info">
+          <span className="label label-default left__label">{this.props.legData.abbreviation}</span>
+          <span className="leg__racer-name">{this.props.legData.racer.name}, {activeClass}</span>
+          <span className="badge leg__timer">
+            <Timer tickTime={this.props.legData.timeCompleted} totalTime={this.props.legData.timeExpected}/>
+          </span>
+        </div>
+        <ProgressBar className="leg__progress" now={60}/>
       </div>
     );
   }
