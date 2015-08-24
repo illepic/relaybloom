55c9fb45cb3b8f6f40d9ea9a6dd17b3d4e884072
Chris Bloom <bloomcb@gmail.com>
2015-08-23 23:41:29 -0700

    set up index.html's to play nice with express

---
+++ relaybloomserver.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var buildPath = path.resolve(__dirname, 'build');
+var publicPath = path.resolve(__dirname, 'public');
+
+//app.get('/', function(req, res){
+//  res.sendFile(publicPath + '/relaybloom.html');
+//});
+
+app.use(express.static('public'));
+app.use(express.static('build'));
+
+io.on('connection', function(socket){
+  socket.on('leg handoff', function(msg){
+    io.emit('leg handoff', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
